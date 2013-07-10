0ffbf82d56cd5e5ad91e2d4577f97c1b2e255f29
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2013-07-10 14:31:28 -0400

    - Added files to IntelliJ project - Created a 'start game' button - Created a 'You win' window upon winning the game

--- script.js
+++ script.js
@@ -1,48 +1,58 @@
 $(document).ready(function() {
-    var sw = function(sq){
+    $('#win').hide();
+
+    $('#start').click(function(){
+        var swapColors = function(sq){
             if(sq.hasClass('unhighlighted')){
                 sq.removeClass('unhighlighted');
-                sq.addClass('highlighted');    
+                sq.addClass('highlighted');
             }
             else{
                 sq.removeClass('highlighted');
-                sq.addClass('unhighlighted');    
-            }    
-        };
-        
-    var puzzle = function(rowcol){
-        for(var i = 0; i < rowcol; i++){
-            $('table').append($('<tr id="'+i+'"/>'));
-            for(var j = 0; j < rowcol; j++){
-                
-                $('#'+i).append($('<td><div id="'+i+'x'+j+'" class="square unhighlighted"/></td>'));
+                sq.addClass('unhighlighted');
             }
-        }  
-    };
-    
-    puzzle(5);
-        
-    $('div').each(function(){
-        if(Math.random() < 0.5){
-            sw($(this));    
-        }    
+        };
+
+        var generatePuzzle = function(rowcol){
+            for(var i = 0; i < rowcol; i++){
+                $('table').append($('<tr id="'+i+'"/>'));
+                for(var j = 0; j < rowcol; j++){
+                    $('#'+i).append($('<td><div id="'+i+'x'+j+'" class="square unhighlighted"/></td>'));
+                }
+            }
+        };
+
+        $('#start').hide();
+
+        generatePuzzle(4);
+
+        $('div').each(function(){
+            if(Math.random() < 0.5){
+                swapColors($(this));
+            }
+        });
+
+        $('table tr td div').click(function(){
+            swapColors($(this));
+
+            var xyCoordinates = $(this).attr('id').split('x');
+            var yCoordinate = Number(xyCoordinates[0]);
+            var xCoordinate = Number(xyCoordinates[1]);
+
+            var upNeighbor = String(yCoordinate - 1) +'x'+ String(xCoordinate);
+            var downNeighbor = String(yCoordinate + 1) + 'x' + String(xCoordinate);
+            var leftNeighbor = String(yCoordinate) + 'x' + String(xCoordinate - 1);
+            var rightNeighbor = String(yCoordinate) + 'x' + String(xCoordinate + 1);
+
+            swapColors($('#' + upNeighbor));
+            swapColors($('#' + downNeighbor));
+            swapColors($('#' + leftNeighbor));
+            swapColors($('#' + rightNeighbor));
+
+            if($('.highlighted').length === $('.square').length){
+                $('table').remove();
+                $('#win').show();
+            }
+        });
     });
-    
-	$('table tr td div').click(function(){
-        sw($(this));
-        
-        var yx = $(this).attr('id').split('x');
-        var y = Number(yx[0]);
-        var x = Number(yx[1]);
-        
-        var upNeighbor = String(y-1) +'x'+ String(x);
-        var downNeighbor = String(y+1) + 'x' + String(x);
-        var leftNeighbor = String(y) + 'x' + String(x-1);
-        var rightNeighbor = String(y) + 'x' + String(x+1);
-        
-        sw($('#'+upNeighbor));
-        sw($('#'+downNeighbor));
-        sw($('#'+leftNeighbor));
-        sw($('#'+rightNeighbor));
-	});
 });
